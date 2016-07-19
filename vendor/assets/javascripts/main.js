c86cb18db8216bc84624259ac90196f216680618
Oju <bioman332@naver.com>
2016-07-19 11:10:15 +0900

    똑같은거 8차 수정 window.onload = function(){} 추가

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -2,9 +2,9 @@
 	Twenty by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-	window.onload = function(){}
-*/
 
+*/
+	window.onload = function(){
 (function($) {
 
 	skel.breakpoints({
@@ -117,3 +117,4 @@
 	});
 
 })(jQuery);
+}
