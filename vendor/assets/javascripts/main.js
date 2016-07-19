841156881a3255fba908c9e2d60010db56f3df01
Oju <bioman332@naver.com>
2016-07-19 11:16:11 +0900

    수정한지 9차

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -4,7 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 */
-	window.onload = function(){
 (function($) {
 
 	skel.breakpoints({
@@ -117,4 +116,3 @@
 	});
 
 })(jQuery);
-}
